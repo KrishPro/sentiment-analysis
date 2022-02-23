27304dd2f86d2890d67db4aaae41a888ca68c66d
Krish Baisoya <krishbaisoya.it2020@gmail.com>
2022-02-23 14:52:21 +0530

    Added some code to setup google colab

--- train.py
+++ train.py
@@ -8,8 +8,35 @@ import torch.nn as nn
 import torch.optim as optim
 from transformers.optimization import get_linear_schedule_with_warmup
 from pytorch_lightning.callbacks import LearningRateMonitor
+from pyngrok import ngrok
+from tensorboard import program
 from data import IMDBDataModule
 from model import Model
+
+# # Setup Colab
+# # Installing torch/xla (TPU), pytorch_lightning, transformers & pyngrok
+# !pip install -q torchtext==0.10 torchaudio==0.9.0 torchvision==0.10.0 tf-estimator-nightly==2.8.0.dev2021122109 earthengine-api==0.1.238 folium==0.2.1
+# !pip install -q cloud-tpu-client==0.10 torch==1.9.0 https://storage.googleapis.com/tpu-pytorch/wheels/torch_xla-1.9-cp37-cp37m-linux_x86_64.whl
+# !pip install -q pytorch_lightning transformers pyngrok
+# # Registering AuthToken for ngrok
+# !wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz
+# !tar zxvf ngrok-stable-linux-amd64.tgz
+# !./ngrok authtoken 1y2MQMr0xLh05Dvbb0dABiNQpAY_3bqEfwwEtM7duDaqwrN93
+# # Registering username & key for kaggle
+# !echo '{"username":"krishbaisoya","key":"d88e0b0654476a2d36bd04dd95bf2fb4"}' > kaggle.json
+# !mkdir ~/.kaggle
+# !mv kaggle.json ~/.kaggle/
+# # Mounting Drive
+# from google.colab import drive
+# drive.mount('/content/drive')
+
+def start_tensorboard(log_dir: str = "/content/drive/MyDrive/Models/sentiment-analysis/fine-tuning/lightning_logs"):
+    tb = program.TensorBoard()
+    tb.configure(argv=[None, '--logdir', log_dir])
+    url = tb.launch()
+    print(f"Tensorflow listening on {url}")
+
+    ngrok.connect(6006)
 
 class TrainModel(Model):
     def __init__(self, learning_rate: float, ultimate_batch_size: int, epochs: int):
