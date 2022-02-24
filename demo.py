fd5a753a678bdff570b5e057c68d432c99a8d2e4
Krish Baisoya <krishbaisoya.it2020@gmail.com>
2022-02-25 03:54:34 +0530

    created a demo & added dist folder to .gitignore

---
+++ demo.py
@@ -0,0 +1,54 @@
+"""
+Written by KrishPro @ KP
+"""
+
+from transformers.models.distilbert.tokenization_distilbert_fast import DistilBertTokenizerFast
+
+from train import TrainModel
+
+
+import torch
+
+
+# print(torch.load("lightning_logs/version_2/checkpoints/epoch=5-step=1877.ckpt", map_location="cpu"))
+
+# TrainModel.load_state_dict()
+
+def load_model():
+    model: TrainModel = TrainModel(learning_rate=1e-6, ultimate_batch_size=1)
+    model.load_state_dict(torch.load("Dist/multi-epoch.ckpt"))
+    model.eval()
+
+    tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+    return model, tokenizer
+
+def take_input():
+    print("Write a review about any movie")
+    review: str = input("=> ").strip().lower()
+    return review
+
+def process_review(review: str, tokenizer: DistilBertTokenizerFast):
+    tokens: dict[str, torch.Tensor] = tokenizer(review, return_tensors="pt")
+    return tokens
+
+def predict():
+    print("Loading Model...")
+    model, tokenizer = load_model()
+    print("\n")
+    review = take_input()
+    tokens = process_review(review, tokenizer)
+    logit: torch.Tensor = model(**tokens)
+    return torch.sigmoid(logit).item()
+
+if __name__ == "__main__":
+    result = predict()
+
+    if not (0.4 < result < 0.6):
+        sentiment: str = "positive" if result > 0.5 else "negative"
+        confidence: float = result if result > 0.5 else (1 - result)
+        print()
+        print(f"Classified review as {sentiment}, {(confidence*100):.3f}%")
+
+    else:
+        print()
+        print("<=== Almost Neutral ===>")
